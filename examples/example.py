cb2da91acf673343efbd060f153bd6490d8091c0
Abhiram Shibu <abhiramshibu1998@gmail.com>
2023-11-19 18:03:53 +0530

    NoIp: Rename module name to lower letters
    Signed-off-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

--- examples/example.py
+++ examples/example.py
@@ -1,6 +1,6 @@
 import sys
 try:
-    from NoIp import NoIp
+    from noip import NoIp
 except ModuleNotFoundError:
     print("NoIp not found, install it by using pip install 'https://github.com/ABHIRAMSHIBU/NoIp/archive/refs/heads/develop.zip'")
     sys.exit(-1)
